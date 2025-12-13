0f72adf1cd645d8c00939db649b7819de1eb441d
bukalla <oumerelfarukee@gmail.com>
2025-12-13 23:05:41 +0300

    adding lists

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,9 +1,24 @@
+import Annaouncement from '@/components/Annaouncement'
+import BigCalendar from '@/components/BigCalander'
+
 import React from 'react'
 
 const ParentPage = () => {
-  return (
-    <div>
+  return ( 
+    <div className='flex-1 p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* Left */}
+      <div className=' w-full xl:w-2/3'> 
       
+      <div className='h-full bg-white p-4 rounded-md'>
+    <h1 className='text-xl font-extrabold'>Schedule (abubeker oumer) </h1>
+    <BigCalendar/>
+      </div>
+      </div>
+      {/* Right */}
+      <div className='w-full xl:w-1/3 flex flex-col gap-8' >
+   
+      <Annaouncement/>
+      </div>
     </div>
   )
 }
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,12 +1,24 @@
+import Annaouncement from '@/components/Annaouncement'
+import BigCalendar from '@/components/BigCalander'
+import EventCalander from '@/components/EventCalander'
 import React from 'react'
 
 const StudentPage = () => {
   return ( 
     <div className='p-4 flex gap-4 flex-col xl:flex-row'>
       {/* Left */}
-      <div className=' w-full xl:w-2/3'></div>
+      <div className=' w-full xl:w-2/3'> 
+      
+      <div className='h-full bg-white p-4 rounded-md'>
+    <h1 className='text-xl font-extrabold'>Schedule (4A)</h1>
+    <BigCalendar/>
+      </div>
+      </div>
       {/* Right */}
-      <div className='w-full xl:w-1/3'></div>
+      <div className='w-full xl:w-1/3 flex flex-col gap-8' >
+      <EventCalander/>
+      <Annaouncement/>
+      </div>
     </div>
   )
 }
